bb8a83da7879e6e688e6f95b90bae01b6dd5479c
jasperou <jou@unc.edu>
2023-12-10 18:22:47 -0500

    api url change

--- frontend/src/app/friends-page/friends.service.ts
+++ frontend/src/app/friends-page/friends.service.ts
@@ -36,7 +36,7 @@ interface FriendRequest {
 export class FriendsService {
   // https://team-d6-comp423-23f.apps.cloudapps.unc.edu/api
   // http://localhost:1560/api
-  private apiUrl = 'http://localhost:1560/api';
+  private apiUrl = 'https://team-d6-comp423-23f.apps.cloudapps.unc.edu/api';
 
   constructor(private http: HttpClient) {}
 
